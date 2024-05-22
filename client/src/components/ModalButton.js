5cf55733ed55f114de19302a535846fd17ff0d2d
SamKallio <sam.kalliomaki@gmail.com>
2024-05-22 13:31:59 +0300

    modalbutton width and height changed to be more fitting

--- client/src/components/ModalButton.js
+++ client/src/components/ModalButton.js
@@ -54,8 +54,8 @@ export default function ModalButton({
           borderRadius: 4,
           border: "2px solid green",
           padding: 0,
-          maxWidth: "180px",
-          maxHeight: "90px",
+          maxWidth: "100px",
+          maxHeight: "70px",
           boxShadow: 2,
         }}
       >
